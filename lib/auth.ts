a880188fe58d9dcca9596bbdc8dafdad35c1bac4
MubashirRazaAnsari <ansarimubashir47@gmail.com>
2025-01-10 06:36:19 +0500

    api key added

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,5 +5,5 @@ export async function auth(request: Request): Promise<boolean> {
   const apiKey = headersList.get('x-api-key')
   
   // Compare with your environment variable
-  return apiKey === process.env.SANITY_API_KEY
+  return apiKey === process.env.SANITY_API_TOKEN
 } 
