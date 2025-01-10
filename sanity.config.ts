72d0ae244d59dc16fc574fa4a4364296ac9283c2
MubashirRazaAnsari <ansarimubashir47@gmail.com>
2025-01-10 06:16:12 +0500

    sanity solve

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,14 +4,9 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schemaTypes} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schema } from './sanity/schemaTypes'
 
 export default defineConfig({
   name: 'default',
@@ -20,7 +15,5 @@ export default defineConfig({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   basePath: '/studio',
   plugins: [structureTool()],
-  schema: {
-    types: schemaTypes,
-  },
+  schema: schema,
 })
