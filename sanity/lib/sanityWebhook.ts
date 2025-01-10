025edff7cb70a69d145ef350478ddc66fbb2b68b
MubashirRazaAnsari <ansarimubashir47@gmail.com>
2025-01-11 02:01:58 +0500

    Test

--- sanity/lib/sanityWebhook.ts
+++ sanity/lib/sanityWebhook.ts
@@ -3,7 +3,7 @@ import { createClient } from 'next-sanity'
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: '2021-03-25',
+  apiVersion: 'v2022-06-30',
   token: process.env.SANITY_API_TOKEN,
   useCdn: false,
   stega: {
