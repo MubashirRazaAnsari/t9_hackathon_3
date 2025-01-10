2e91b084954ea99c10d110a47b8322a9e207c848
MubashirRazaAnsari <ansarimubashir47@gmail.com>
2025-01-10 06:18:37 +0500

    type error solve

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -3,7 +3,7 @@
 import { useLiveQuery } from 'next-sanity/preview'
 import { client } from './client'
 
-export function usePreview(query: string, params?: Record<string, unknown>) {
+export function usePreview(query: string, params: string) {
   return useLiveQuery(query, params, {
     client,
     token: process.env.SANITY_API_KEY,
